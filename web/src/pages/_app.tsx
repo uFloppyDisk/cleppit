4680428bf132779e00cad6bd8568e5aa970d8b47
Pawel Bartusiak <floppydiskproductions@outlook.com>
2023-04-07 20:24:56 -0700

    Started fix for login state caching;

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,11 +2,52 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from '../theme';
 import { AppProps } from 'next/app';
-import { Provider, Client, cacheExchange, fetchExchange } from 'urql';
+import { Provider, Client, fetchExchange } from 'urql';
+import { QueryInput, cacheExchange, Cache } from '@urql/exchange-graphcache';
+import {
+    LoginMutation,
+    MeDocument,
+    MeQuery,
+} from '../generated/graphql/graphql';
+
+function fdUpdateQuery<Result, Query>(
+    result: any,
+    cache: Cache,
+    input: QueryInput,
+    fn: (res: Result, query: Query) => Query
+) {
+    return cache.updateQuery(input, (data) => fn(result, data as any) as any);
+}
 
 const client = new Client({
     url: 'http://localhost:4000/graphql',
-    exchanges: [cacheExchange, fetchExchange],
+    exchanges: [
+        cacheExchange({
+            updates: {
+                Mutation: {
+                    login: (result: LoginMutation, args, cache, info) => {
+                        fdUpdateQuery<LoginMutation, MeQuery>(
+                            result,
+                            cache,
+                            {
+                                query: MeDocument,
+                            },
+                            (result, query) => {
+                                if (result.login.errors) {
+                                    return query;
+                                } else {
+                                    return {
+                                        me: result.login.user,
+                                    };
+                                }
+                            }
+                        );
+                    },
+                },
+            },
+        }),
+        fetchExchange,
+    ],
     fetchOptions: {
         credentials: 'include',
     },
